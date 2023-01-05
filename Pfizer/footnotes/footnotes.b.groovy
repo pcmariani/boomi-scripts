f446ce7c684e181b17775534b66d45993b57b6ab
Pete Mariani <pcmariani@gmail.com>
2023-01-05 17:24:42 -0500

    Updated footenotes script

--- Pfizer/footnotes/footnotes.b.groovy
+++ Pfizer/footnotes/footnotes.b.groovy
@@ -13,30 +13,46 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     Properties props = dataContext.getProperties(i);
 
     def root = new JsonSlurper().parse(is)
-
-  println JsonOutput.prettyPrint(JsonOutput.toJson(root.footnotes))
+    // println JsonOutput.prettyPrint(JsonOutput.toJson(root.footnotes))
 
     def footnotes = []
     def footnotesVals
     def footnotesRefs
+    def hasFooterRow = true
+    def footnotesPerTableGroupArr = []
 
     if (root.footnotes) {
-        footnotes = new JsonSlurper().parseText(root.footnotes)
-        footnotesVals = footnotes[0]
-        footnotesRefs = footnotes[1]
+        footnotes = new JsonSlurper().parseText("[" + root.footnotes + "]")
+        // println footnotes[0][0].keySet()[0]
+        if (footnotes[0][0].keySet()[0] != "footer-row") {
+            hasFooterRow = false
+            footnotes = new JsonSlurper().parseText(root.footnotes)
+            footnotesVals = footnotes[0]
+            footnotesRefs = footnotes[1]
+        }
     }
+    // println "hasFooterRow: " + hasFooterRow
+    // println footnotes
+
+    if (hasFooterRow) {
+        footnotesPerTableGroupArr = getFootnotesPerTableGroupArr(footnotes)
+        // println JsonOutput.prettyPrint(JsonOutput.toJson(footnotesPerTableGroupArr))
+    }
+    
 
     def html = root.query_html
     def htmlroot = new XmlSlurper().parseText(html)
+    // println htmlroot.body.tablegroup.size()
+    // println footnotesPerTableGroupArr.size()
 
     def tableGroupsArr = []
 
-    htmlroot.body.tablegroup.each { tableGroup ->
+    htmlroot.body.tablegroup.eachWithIndex { tableGroup, tgnum ->
         def tableGroupMap = [:]
 
         tableGroupMap['query_html'] = "<html>\n<body>\n" + XmlUtil.serialize(tableGroup).replaceFirst("<\\?xml version=\"1.0\".*\\?>", "") + "</body>\n</html>"
 
-        if (footnotes) {
+        if (!hasFooterRow && footnotes) {
             def newFootnotesValsMap = [:]
             def newFootnotesRefsMap = [:]
             footnotesRefs.each { k, v ->
@@ -56,32 +72,30 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
                 tableGroupMap['footnotes'] = JsonOutput.toJson([newFootnotesValsMap, newFootnotesRefsMap])
             }
         }
+        else if (footnotes) {
+            tableGroupMap['footnotes'] = JsonOutput.toJson(footnotesPerTableGroupArr[tgnum]['footnotes'])
+            tableGroupMap['footer-row'] = footnotesPerTableGroupArr[tgnum]['footer-row']
+        }
 
         tableGroupMap['tableName'] = tableGroup.h3 as String
 
         tableGroupsArr << tableGroupMap
     }
+
     // tableGroupsArr.collect{
-      // println it.footnotes + "\n"
+    //   println "footer-row:\n" + it['footer-row'] + "\n"
+    //   println "footnotes:\n" + it['footnotes'] + "\n"
     // }
 
     def builder = new JsonBuilder()
     def newroot = builder {
         MD5 (root.MD5)
         Table_and_Footnotes tableGroupsArr.collect{
-            if (it.footnotes) {
-                [
-                  'query_html': it.query_html,
-                  'footnotes': it.footnotes,
-                  'tablename': it.tableName
-                ]
+            def outMap = [:]
+            for (key in ["query_html","footnotes","footer-row","tableName"]) {
+                if (it[key] && it[key] != "null") outMap[key] = it[key]
             }
-            else {
-                [
-                  'query_html': it.query_html,
-                  'tablename': it.tableName
-                ]
-            }
+            outMap
         }
     }
 
@@ -90,4 +104,35 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
 
     is = new ByteArrayInputStream(outData.getBytes("UTF-8"));
     dataContext.storeStream(is, props);
+}
+
+
+def getFootnotesPerTableGroupArr(footnotes) {
+
+    def footnotesPerTableGroupArr = []
+    def footnotesPerTableGroupCounter = 0
+    def footnotesPerTableGroupCounterPrev = -1
+    def footnotesPerTableGroupMap = [:]
+
+    def footnotesValsMap = [:]
+    def footnotesRefsMap = [:]
+
+    footnotes.eachWithIndex { item, j ->
+      item.each {
+        if (item[0].keySet()[0].contains("footer-row")) {
+          footnotesPerTableGroupMap = [:]
+          footnotesPerTableGroupMap.'footer-row' = it.'footer-row'
+          footnotesPerTableGroupCounter++
+        }
+        else {
+          footnotesPerTableGroupMap.'footnotes' = item
+          footnotesPerTableGroupCounterPrev = footnotesPerTableGroupCounter
+        } 
+      }
+      if (footnotesPerTableGroupCounter != footnotesPerTableGroupCounterPrev) {
+        footnotesPerTableGroupArr[footnotesPerTableGroupCounter-1] = footnotesPerTableGroupMap
+      }
+    }
+    
+    return footnotesPerTableGroupArr
 }
